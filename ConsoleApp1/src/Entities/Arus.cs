bf23e9be6313bd6c4062ddc5eea836319dd04e7d
CharlesSouzaAlmeid <charlestark@gmail.com>
2022-01-27 23:33:24 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/src/Entities/Arus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.src.Entities
+{
+    public class Arus
+    {
+        public Arus(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public string Name;
+        public int Level;
+        public string HeroType;
+    }
+}
---
+++ ConsoleApp1/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1.src.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arus hero = new Arus("Arus", 23, "Knight");
+
+
+            Console.ReadLine();
+        }
+    }
+}
